89fa3b12c8d321f765d84af401aa694cbd87c802
Amine <01aminetahiri01@gmail.com>
2026-07-03 13:11:27 +0100

    second add

--- weather.py
+++ weather.py
@@ -1,1 +1,12 @@
-import requests
+# import requests
+# # url = "e159e7f9ee473c14b38b5618f779c17a"
+# url = "https://e159e7f9ee473c14b38b5618f779c17a"
+# response = requests.get(url)
+# if response.status_code == 200:
+#     print("recive data succecfully")
+# else:
+#     print("something went wrong")
+# data = response.json()
+# print(data)
+print("first time working with git & understand it")
+print("second change")
