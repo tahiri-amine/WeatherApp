a53f759b7d7f503a3c54e39c04ad8088b2d41982
Amine <01aminetahiri01@gmail.com>
2026-07-16 19:09:47 +0100

    a basic version of weather app

--- weather.py
+++ weather.py
@@ -1,2 +1,7 @@
-
+import requests
+city = input("enter a city name:")
+url = f"https://wttr.in/{city}?format=j1"
+r = requests.get(url)
+data = r.json()
+print(f"the temperature in {city} is: {data['current_condition'][0]['temp_C']} deg")
 
