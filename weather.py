89cdd29c9388f6759e660fa54070b4ec05b8f476
Amine <01aminetahiri01@gmail.com>
2026-07-17 21:37:53 +0100

    feet: init weather CLI with temperature,humidity,wind,ect..and description

--- weather.py
+++ weather.py
@@ -1,7 +1,33 @@
 import requests
-city = input("enter a city name:")
-url = f"https://wttr.in/{city}?format=j1"
-r = requests.get(url)
-data = r.json()
-print(f"the temperature in {city} is: {data['current_condition'][0]['temp_C']} deg")
+import argparse
+import datetime
+
+try:
+    parser= argparse.ArgumentParser()
+    parser.add_argument("-n",help="a city name",required=True)
+    args = parser.parse_args()
+   
+    # handle the number ginving errour a buil in problem
+    url = f"https://wttr.in/{args.n}?format=j1"
+    r = requests.get(url) 
+    if r.status_code == 200:
+        data = r.json()
+        temp = data['current_condition'][0]['temp_C']
+        humidity = data['current_condition'][0]["humidity"]
+        windSpeed = data['current_condition'][0]["windspeedKmph"]
+        precip = data['current_condition'][0]["precipMM"]
+        weatherdesc = data['current_condition'][0]["weatherDesc"][0]["value"]
+        winddir = data['current_condition'][0]["winddir16Point"]
+        print(f"""[--===--{args.n.capitalize()}--===-- ]""")
+        print(f"Day:{datetime.date.today()} | Time:{datetime.datetime.now().strftime("%H:%M")}")
+        print(f"Temperature:{temp}°C")
+        print(f"Humidity:{humidity}%")
+        print(f"Precipitation:{precip}MM")
+        print(f"WindSpeed:{windSpeed}Kmph | Direction:{winddir}")
+        print(f"WeatherDescription:{weatherdesc}")
+    else:
+        print(f"NameError: there is no city with the name '{args.n}'")
+
+except (NameError,requests.exceptions.JSONDecodeError):
+    print(f"NameError: there is no city with the name '{args.n}'") # question in try/except should i print or raise the error?
 
